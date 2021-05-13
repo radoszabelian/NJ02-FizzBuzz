c64281ed379d69fc58be125c8a30d5baae0b5bb3
Belian <radoszabelian@gmail.com>
2021-05-13 14:43:07 +0200

    Add project files.

---
+++ NJ02-FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NJ02_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                string r = "";
+                if (n % 3 == 0) r += "Fizz ";
+                if (n % 5 == 0) r += "Buzz";
+                if (r.Length == 0) r += n;
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
